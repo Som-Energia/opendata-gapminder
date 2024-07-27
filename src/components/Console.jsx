f2b166e28eb39c521557542e8c4bf72cdf8b4218
David García Garzón <david.garcia@somenergia.coop>
2024-07-27 15:05:41 +0200

    💄 nicer loading and error reporting
    using somenergia-ui

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -6,6 +6,8 @@ import TextField from '@mui/material/TextField'
 import PlayIcon from '@mui/icons-material/PlayArrow'
 import StopIcon from '@mui/icons-material/Stop'
 import ColorModeButton from '@somenergia/somenergia-ui/ColorModeButton'
+import Loading from '@somenergia/somenergia-ui/Loading'
+import DizzyError from '@somenergia/somenergia-ui/DizzyError'
 import OpenData from '../services/opendata'
 
 function t(x) {
@@ -25,25 +27,38 @@ function MetricSelector({ label, onChange, value, options }) {
 }
 
 function Console() {
-  const [loadingOpenData, setLoadingOpenData] = React.useState(false)
+  const notstarted = 'notstarted'
+  const inprogress = 'inprogress'
+  const done = 'done'
+  // Any other value is an exception
+  const [loadingOpenData, setLoadingOpenData] = React.useState(notstarted)
   const [playing, setPlaying] = React.useState(true)
   const [xMetric, setXMetric] = React.useState('')
   const [yMetric, setYMetric] = React.useState('')
   const [rMetric, setRMetric] = React.useState('')
   const [options, setOptions] = React.useState([])
 
-  React.useEffect(()=>{
-    setLoadingOpenData(true)
-    if (loadingOpenData) return // avoid double load in dev mode
-    OpenData.retrieveData().then((d)=> {
-      console.log("data loaded", d)
-      const options = OpenData.metricOptions().map(({value, text})=>({value, label: text}))
-      setOptions(options)
-      setXMetric("members")
-      setYMetric("contracts")
-      setRMetric("members_change")
-    })
-  }, [])
+  React.useEffect(() => {
+    if (loadingOpenData !== notstarted) return // avoid double load in dev mode
+    setLoadingOpenData(inprogress)
+    OpenData.retrieveData()
+      .then((d) => {
+        console.log('data loaded', d)
+        const options = OpenData.metricOptions().map(({ value, text }) => ({
+          value,
+          label: text,
+        }))
+        setOptions(options)
+        setXMetric('members')
+        setYMetric('contracts')
+        setRMetric('members_change')
+        setLoadingOpenData(done)
+      })
+      .catch((e) => {
+        console.log(Object.keys(e))
+        setLoadingOpenData(e)
+      })
+  }, [loadingOpenData])
 
   function togglePlaying() {
     setPlaying((wasPlaying) => !wasPlaying)
@@ -69,21 +84,45 @@ function Console() {
       height="calc(100vh - 20pt)"
     >
       <Stack direction="row" sx={{ flexGrow: 1 }}>
-        <svg width="100%">
-          <rect
-            x="0"
-            y="0"
-            width="100%"
+        {loadingOpenData === notstarted || loadingOpenData === inprogress ? (
+          <Loading />
+        ) : loadingOpenData === done ? (
+          <svg width="100%">
+            <rect
+              x="0"
+              y="0"
+              width="100%"
+              height="100%"
+              fill="#333"
+              stroke="green"
+              strokeWidth="10"
+            />
+          </svg>
+        ) : (
+          <Stack
+            direction="column"
             height="100%"
-            fill="#333"
-            stroke="green"
-            strokeWidth="10"
-          />
-        </svg>
+            width="100%"
+            justifyContent="center"
+            alignContent="center"
+            alignItems="center"
+            gap="1rem"
+          >
+            <DizzyError />
+            <div>{loadingOpenData + ''}</div>
+            <Button variant="contained" color="error">
+              {t('RELOAD_DATA')}
+            </Button>
+          </Stack>
+        )}
       </Stack>
       <Stack direction="row" margin="1rem 1rem 2rem" gap="1rem">
         <Stack direction="row" gap="1rem">
-          <Button variant="contained" onClick={togglePlaying} startIcon={playing ? <StopIcon /> : <PlayIcon />}>
+          <Button
+            variant="contained"
+            onClick={togglePlaying}
+            startIcon={playing ? <StopIcon /> : <PlayIcon />}
+          >
             {playing ? t('PAUSE') : t('PLAY')}
           </Button>
         </Stack>
