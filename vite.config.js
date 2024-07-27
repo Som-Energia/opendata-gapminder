1744e2630c4524056b43994296fd7e34ed529be9
David García Garzón <david.garcia@somenergia.coop>
2024-07-28 01:40:06 +0200

    🏗️ using BASE_URL in production

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteyaml from '@modyfi/vite-plugin-yaml'
 import dsv from '@rollup/plugin-dsv'
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    dedupe: Object.keys(pkg.dependencies),
-  },
-  plugins: [react(), viteyaml(), dsv()],
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
-  server: {
-    port: 8001,
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), 'BASE_URL') }
+  return {
+    base: process.env.BASE_URL,
+    resolve: {
+      dedupe: Object.keys(pkg.dependencies),
+    },
+    plugins: [react(), viteyaml(), dsv()],
+    define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+    server: {
+      port: 8001,
+    },
   }
 })
